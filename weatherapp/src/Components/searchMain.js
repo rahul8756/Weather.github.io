427cb60c48daa3970b81784743802a89124e71fc
Your Name <rt1169164@gmail.com>
2022-08-23 08:02:20 +0530

    new file:   weatherapp/.gitignore 	new file:   weatherapp/README.md 	new file:   weatherapp/package-lock.json 	new file:   weatherapp/package.json 	new file:   weatherapp/public/index.html 	new file:   weatherapp/src/App.css   new file:   weatherapp/src/App.js 	new file:   weatherapp/src/Components/searchMain.js 	new file:   weatherapp/src/Components/style.css 	new file:   weatherapp/src/Components/weatherDetails.js   new file:   weatherapp/src/index.css 	new file:   weatherapp/src/index.js

---
+++ weatherapp/src/Components/searchMain.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import "../Components/style.css";
+import WeatherDetails from "./weatherDetails";
+
+function SearchMain() {
+  const [searchTerm, setSearchTerm] = useState("Lucknow");
+  const [tempInfo, setTempInfo] = useState({});
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=metric&appid=de0d53976a10330b2706134a2b65dbd1`;
+      let res = await fetch(url);
+      let data = await res.json();
+      const { temp, humidity, pressure } = data.main;
+      const { main: weatherType } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weatherType,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+
+      setTempInfo(myNewWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  });
+
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="Search city.."
+            id="search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button className="searchButton" onClick={getWeatherInfo}>
+            Search
+          </button>
+        </div>
+      </div>
+
+      <WeatherDetails {...tempInfo} />
+    </>
+  );
+}
+
+export default SearchMain;
